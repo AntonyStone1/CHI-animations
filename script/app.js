b67bfe666370c56828f98a21c4e1e65a1a2ebb2a
Anton Kameniev <anton.kameniev@chisw.com>
2021-11-15 18:06:15 +0200

    custom array methods

--- script/app.js
+++ script/app.js
@@ -74,7 +74,13 @@ porgressPrecent.addEventListener('change', () => {
 })
 
 
-/////////// Array methods //////////////
+/////////// Array methods //////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+///////////////////////////////////////  \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+//////////////////////////////////////    \\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+/////////////////////////////////////      \\\\\\\\\\\\\\\\\\\\\\\\\\\\
+////////////////////////////////////        \\\\\\\\\\\\\\\\\\\\\\\\\\\
+///////////////////////////////////          \\\\\\\\\\\\\\\\\\\\\\\\\\
+
 
 
 
@@ -82,10 +88,7 @@ let array = [1,2,3,4,5,4,-7,-12,-45,3,2,3,4,5,9,8,10,5,5,5]
 // console.log(array.map((item) => item === 3));;
 
 Array.prototype.customMap = function(callback, thisArgs) {
-    if (this == null) {
-        throw new Error("Cant iterate over undefined or null");
-    }
-
+    
     let O = Object(this);    
 
     if (typeof callback !== "function") {
@@ -100,20 +103,22 @@ Array.prototype.customMap = function(callback, thisArgs) {
     }  
     return result;
 }
+
+
 // array.customMap((item, index, arr) => console.log(item, index, arr));
 
 Array.prototype.customForEach = function(callback, thisArgs) {
     if (this == null) {
         throw new Error("Cant iterate over undefined or null");
     }
-    let O = Object(this);
+    let obj = Object(this);
 
     if (typeof callback !== "function") {
       throw new Error("Callback is not a function");
     }
 
     for (let i = 0; i < O.length; i++) {
-        if (i in O) {
+        if (i in obj) {
             result[i] = callback.call(thisArgs, this[i], i, this)
         }
     }
@@ -136,7 +141,7 @@ Array.prototype.customFilter = function(callback, thisArgs) {
     if (this == null) {
         throw new Error("Cant iterate over undefined or null");
     }
-    let O = Object(this);
+    let obj = Object(this);
 
     if (typeof callback !== "function") {
       throw new Error("Callback is not a function");
@@ -144,7 +149,7 @@ Array.prototype.customFilter = function(callback, thisArgs) {
 
     const result = [];
     for (let i = 0; i < O.length; i++) {
-        if (i in O) {
+        if (i in obj) {
             if (callback.call(thisArgs, this[i], i, this)) {
                 result.push(this[i])
             }
@@ -179,4 +184,185 @@ Array.prototype.customReduce = function(callback, initialValue) {
     return acc;
 }
 
-console.log([1,2,,,,,4].customReduce((acum, item) => acum += item));
+// console.log([1,2,,,,,4].reduce((acum, item) => acum += item));
+// console.log([1,2,,,,,4].customReduce((acum, item) => acum += item));
+
+const randomNum = (num) => 2 + num;
+
+Array.prototype.customAt = function(value) {
+    value = Number(value);
+    if (value !== undefined && !isNaN(value)) {
+        return (value >= 0) ? this[value] : this[this.length + value];  
+    } else {
+        return this[0];
+    }
+        
+}
+
+// console.log([12,3,46,2,34].at(2));
+// console.log([12,3,46,2,34].customAt(2));
+
+
+
+Array.prototype.customConcat = function(thisArgs) {    
+    const result = this;
+    for (let j = 0; j < arguments.length; j++) {
+        if (arguments[j].length !== undefined) { 
+            for (let i = 0; i < arguments[j].length; i++) {
+                result[result.length] = arguments[j][i];
+            }
+        } else {
+            result[result.length] = arguments[j];
+        }
+    }
+    
+    return result;
+}
+
+// const array1 = ['a', 'b', 'c'];
+// const array2 = ['d', 'e', 'f'];
+// const array3 = ['1', 2, '3'];
+// console.log(array1.concat(array2, array3, [1,[2,3]],{1:2, 2:3}, [22,3,4,5,{1:22, 2:33}]));
+// console.log(array1.customConcat(array2, array3, [1,[2,3]],{1:2, 2:3}, [22,3,4,5,{1:22, 2:33}]));
+
+Array.prototype.customEvery = function(callback, thisArgs) {    
+    let obj = Object(this);
+
+    if (typeof callback !== "function") {
+      throw new Error("Callback is not a function");
+    }
+    let result = false;
+    for (let i = 0; i < obj.length; i++) {
+        if (i in obj) {
+            if (callback.call(thisArgs, this[i], i, this)) {
+                result = true;
+            } else {
+                result = false;
+            }
+        } 
+    }
+    return result;
+}
+
+// const isBelowThreshold = (currentValue) => currentValue < 40;
+
+// const array1 = [1, 30, 39, 29, 10, 40];
+
+// console.log(array1.every(isBelowThreshold));
+// console.log(array1.customEvery(isBelowThreshold));
+
+
+Array.prototype.customPop = function() {
+    let result = this[this.length - 1];
+    this.length = this.length - 1;
+    return result;
+}
+
+// let popArr = [1, 30, 39, 29, 10, 40];
+// console.log(popArr);
+// console.log(popArr.pop());
+// console.log(popArr);
+// console.log(popArr.customPop());
+// console.log(popArr);
+
+Array.prototype.customPush = function(value) {
+    if (value !== undefined) {
+        this[this.length] = value;
+    }    
+    return this.length;
+}
+// let pushArr = [1, 30, 39, 29, 10, 40];
+// console.log(pushArr);
+// console.log(pushArr.push(5));
+// console.log(pushArr);
+// console.log(pushArr.customPush(5));
+// console.log(pushArr);
+
+
+
+Array.prototype.customSort = function() {
+    return quickSort;
+    function quickSort(arr) {
+        if (arr.length === 1) {
+            console.log(arr);
+            return arr;            
+        } else {
+            let q = arr[Math.round(arr.length / 2)]
+            let left = [];
+            let center = [];
+            let right = [];
+
+            for (let i = 0; i < array.length; i++) {
+                let item = array[i];
+                if(item > q)
+    			{
+    				right.push(item);
+    			}
+    			else if (item < q)
+    			{
+    				left.push(item);
+    			}
+    			else
+    			{
+    				center.push(item);
+    			}
+            }
+            return quickSort(left).concat(center).concat(quickSort(right));
+        }
+    }    
+}
+
+let arrToSort = [1, 30, 39, 29, 10, 40];
+console.log(arrToSort.customSort());
+
+
+
+// Array.prototype.sort1 = function(compareFn) {
+//     return mergeSort(this)
+//     // Split the array into halves and merge them recursively 
+//     function mergeSort(arr) {
+//         if (arr.length === 1) {
+//             // return once we hit an array with a single item
+//             return arr
+//         }
+//         const middle = Math.floor(arr.length / 2) // get the middle item of the array rounded down
+//         const left = arr.slice(0, middle) // items on the left side
+//         const right = arr.slice(middle) // items on the right side
+//         return merge(
+//             mergeSort(left),
+//             mergeSort(right)
+//         )
+//     }
+//     // compare the arrays item by item and return the concatenated result
+//     function merge(left, right) {
+//         let result = []
+//         let indexLeft = 0
+//         let indexRight = 0
+//         while (indexLeft < left.length && indexRight < right.length) {
+//             //compareFn ? compareFn =()=> left[indexLeft] < right[indexRight] : compareFn
+//             let _left = left[indexLeft]
+//             let _right = right[indexRight]
+//             if (compareFn)
+//                 compareFn = composeCompareFn(compareFn(left, right))
+//             compareFn = (l, r) => l < r
+//             if (compareFn(_left, _right)) {
+//                 result.push(left[indexLeft])
+//                 indexLeft++
+//             } else {
+//                 result.push(right[indexRight])
+//                 indexRight++
+//             }
+//         }
+//         return result.concat(left.slice(indexLeft)).concat(right.slice(indexRight))
+//     }
+//     function composeCompareFn(compareResult) {
+//         if (Math.sign(compareResult) == -1)
+//             return false
+//         if (Math.sign(compareResult) == 1)
+//             return true
+//         if (compareResult == 0)
+//             return false
+//     }
+// }
+
+// console.log(arrToSort.sort1());
